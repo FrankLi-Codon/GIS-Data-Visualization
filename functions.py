745300394a8693d93a3d137050269ecfc7462aa6
Frank Li <fr.li@mail.utoronto.ca>
2018-03-13 23:00:58 -0400

    Final Visualization

--- functions.py
+++ functions.py
@@ -1,8 +1,12 @@
-'''function to return county from serach term'''
-
-def find_county(serach_list):
-    import geocoder
-    serach_result = geocoder.bing(serach_list, method = 'batch')
-    county_list = [i.county for i in serach_result]
-    county_dict = {zip(serach_list, county_list)}
-    return county_dict
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=-1):
+    import numpy as np
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    import matplotlib.gridspec as gridspec
+    import matplotlib.colors as mcolors
+    if n == -1:
+        n = cmap.N
+    new_cmap = mcolors.LinearSegmentedColormap.from_list(
+         'trunc({name},{a:.2f},{b:.2f})'.format(name=cmap.name, a=minval, b=maxval),
+         cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
