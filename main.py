11008dfa0edadf191367638dbcd1a7b8dd146f78
Frank Li <fr.li@mail.utoronto.ca>
2018-03-16 00:55:27 -0400

    Finished Visualization

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=-1):
-    import numpy as np
-    import matplotlib as mpl
-    import matplotlib.pyplot as plt
-    import matplotlib.gridspec as gridspec
     import matplotlib.colors as mcolors
+    import numpy as np
     if n == -1:
         n = cmap.N
     new_cmap = mcolors.LinearSegmentedColormap.from_list(
